76102c52c4e10c42ca7cbdbefcafd90b5af92c63
batalova90 <batalova.av@gmail.com>
2022-12-29 12:20:06 +0300

    added schema search_xml

--- tests/test_api/enum_api.py
+++ tests/test_api/enum_api.py
@@ -5,7 +5,8 @@ class EnumAPI(enum.Enum):
     GEOCODE_JSON = "https://nominatim.openstreetmap.org/search?q=%CE%91%CE%B3%CE%AF%CE%B1+%CE%A4%CF%81%CE%B9%CE%AC%CE%B4%CE%B1%2C+%CE%91%CE%B4%CF%89%CE%BD%CE%B9%CE%B4%CE%BF%CF%82%2C+Athens%2C+Greece&format=geocodejson"
     REVERSE_JSON = "https://nominatim.openstreetmap.org/reverse?format=geocodejson&lat=38.005169699999996&lon=23.72949633941048"
     REVERSE_ZOOM = "https://nominatim.openstreetmap.org/reverse?format=geocodejson&lat=56.91231&lon=92.85655&zoom="
-    SEARCH_OBJECT = "https://nominatim.openstreetmap.org/?addressdetails=0&q="
+    SEARCH_OBJECT = "https://nominatim.openstreetmap.org/?addressdetails=1&q="
+    SEARCH_XML = 'https://nominatim.openstreetmap.org/search?format=xml&addressdetails=1&q='
     REVERSE_OBJECT = "https://nominatim.openstreetmap.org/reverse?format=json&lat="
     LOOKUP_JSON = "https://nominatim.openstreetmap.org/lookup?format=json&osm_ids=W"
 
@@ -21,3 +22,4 @@ class EnumMessagesError(enum.Enum):
     INVALID_OSM_ID = "OSM_ID objects search and reverse requests is not equal"
     INVALID_LONGITUDE_LOOKUP = "Invalid longitude"
     INVALID_LATITUDE_LOOKUP = "Invalid latitude"
+    INVALID_XML_FORMAT = "Invalid xml-format"
---
+++ tests/test_api/test_search/search_schema_xml.py
@@ -0,0 +1,22 @@
+import pydantic_xml as pxml
+from typing import Optional
+
+
+class Place(pxml.BaseXmlModel, tag="place"):
+    house_number: int = pxml.element()
+    road: str = pxml.element()
+    hamlet: str = pxml.element()
+    town: str = pxml.element()
+    village: str = pxml.element()
+    city: str = pxml.element()
+    ISO3166_lvl18: Optional[str] = pxml.element(tag="ISO3166-2-lvl8")
+    state_district: str = pxml.element()
+    state: str = pxml.element()
+    ISO3166_lvl14: Optional[str] = pxml.element(tag="ISO3166-2-lvl4")
+    postcode: str = pxml.element()
+    country: str = pxml.element()
+    country_code: str = pxml.element()
+
+
+class SearchResults(pxml.BaseXmlModel, tag="searchresults"):
+    place: Place = pxml.element(tag='place')
